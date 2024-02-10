1e8d6232bab4b665e366f5225345c297e889af66
Xiphereal <lozanojalex@gmail.com>
2024-02-10 12:49:23 +0100

    revert "feat: what Items are owned by the Traveler need to be declared". Esto no me cuadra para nada, ni consigo hacerlo funcionar

--- Code/Views/TravelerView.cs
+++ Code/Views/TravelerView.cs
@@ -1,7 +1,5 @@
 using Godot;
-using Godot.Collections;
 using Models;
-using System.Linq;
 
 namespace Views;
 
@@ -10,15 +8,11 @@ public partial class TravelerView : Control
     [Export(PropertyHint.Range, "0, 99")]
     private int startingWith = 6;
 
-    [Export(PropertyHint.ArrayType)]
-    private Array<ASdgaksdg> items = new();
-
     public Traveler Traveler { get; private set; }
 
     public override void _Ready()
     {
         Traveler = new Traveler(startingWith);
-        Traveler.Owns(items.Select(x => x.Item));
     }
 
     public override void _Process(double delta)
