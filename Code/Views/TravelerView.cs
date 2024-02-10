5e15250a16c00055fc6e94f4cd6721bf63c26573
Xiphereal <lozanojalex@gmail.com>
2024-02-10 12:49:08 +0100

    feat: what Items are owned by the Traveler need to be declared

--- Code/Views/TravelerView.cs
+++ Code/Views/TravelerView.cs
@@ -1,5 +1,7 @@
 using Godot;
+using Godot.Collections;
 using Models;
+using System.Linq;
 
 namespace Views;
 
@@ -8,11 +10,15 @@ public partial class TravelerView : Control
     [Export(PropertyHint.Range, "0, 99")]
     private int startingWith = 6;
 
+    [Export(PropertyHint.ArrayType)]
+    private Array<ASdgaksdg> items = new();
+
     public Traveler Traveler { get; private set; }
 
     public override void _Ready()
     {
         Traveler = new Traveler(startingWith);
+        Traveler.Owns(items.Select(x => x.Item));
     }
 
     public override void _Process(double delta)
