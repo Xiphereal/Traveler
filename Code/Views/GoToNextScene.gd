99296f9126d6087b80386834f4baa43e556b2a29
Xiphereal <lozanojalex@gmail.com>
2024-02-21 23:15:06 +0100

    feat: modify scene change to be made based on scenes URI rather than PackedScenes. PackedScenes seem to have a global state, and trying to change to a previous exited scene is not possible 'cause it seems to be disposed.

--- Code/Views/GoToNextScene.gd
+++ Code/Views/GoToNextScene.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var next : PackedScene
+@export var pathToNextScene : String
 
 func _go_to_next_scene():
-	get_tree().change_scene_to_packed(next)
+	get_tree().change_scene_to_file(pathToNextScene)
