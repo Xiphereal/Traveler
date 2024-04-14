4b72026c0b63601dafdac5b505dbac8fde5521c2
Xiphereal <lozanojalex@gmail.com>
2024-04-14 18:56:53 +0200

    feat: add more stops to roadmap to create a more meaningful journey

--- Code/Models/Roadmap.cs
+++ Code/Models/Roadmap.cs
@@ -5,18 +5,42 @@ namespace Models
 {
     public class Roadmap
     {
+        private const string Intro = "intro";
+        private const string Preparation = "preparation";
+        private const string Travelling = "travelling";
+        private const string Request = "request";
+        private const string Delivery = "delivery";
+        private const string End = "end";
+
         private readonly List<string> stops = new()
         {
-            "intro",
-            "preparation",
-            "travelling",
-            "preparation",
-            "travelling",
-            "request",
-            "preparation",
-            "travelling",
-            "delivery",
-            "end"
+            Intro,
+            Preparation,
+            Travelling,
+            Preparation,
+            Travelling,
+
+            Request, // A
+            Preparation,
+            Travelling,
+            Delivery, // A
+
+            Preparation,
+            Travelling,
+            Request, // B
+            Preparation,
+            Travelling,
+            Preparation,
+            Travelling,
+            Delivery, // B
+
+            Request, // C
+            Preparation,
+            Travelling,
+            Preparation,
+            Travelling,
+
+            End
         };
 
         private int stop = 0;
